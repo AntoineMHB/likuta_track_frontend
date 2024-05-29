925edd14e1397f75806e28fecf3cd48635122bce
AntoineMHB <antoinemuhumbania@gmail.com>
2024-05-29 18:36:53 +0200

    ...

---
+++ src/components/SidebarData.js
@@ -0,0 +1,24 @@
+import { HomeIcon } from '@heroicons/react/24/solid'
+export const SidebarData = [
+    {
+        title: "Home",
+        icon: HomeIcon,
+        link: "/"
+
+    },
+
+     {
+        title: "Dashboard",
+        //icon: ,
+        link: "/"
+
+    },
+
+     {
+        title: "Home",
+        icon: HomeIcon,
+        link: "/"
+
+    }
+]
+ 
