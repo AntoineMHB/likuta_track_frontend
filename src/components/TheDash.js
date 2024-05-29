a337c2beaf6b0c319d57cbf155027a1aa9e0c652
AntoineMHB <antoinemuhumbania@gmail.com>
2024-05-29 16:17:50 +0200

    ...

--- src/components/TheDash.js
+++ src/components/TheDash.js
@@ -20,7 +20,6 @@ const TheDash = () => {
     const [userName, setUserName] = useState('');
     const { budgets: initialBudgets, expenses } = useLoaderData();
     const [budgets, setBudgets] = useState(initialBudgets);
-
     const [showPopup, setShowPopup] = useState(false);
 
     const togglePopup = () => {
@@ -29,16 +28,15 @@ const TheDash = () => {
 
     const handleAddBudget = (newBudget) => {
         console.log('New budget added:', newBudget);
-        addBudget(newBudget); // Add the new budget to the sate
-        togglePopup(); // Close the popup after adding a new 
+        addBudget(newBudget);
+        togglePopup();
     };
 
     useEffect(() => {
-        // Retrieve user data from local storage
         const userData = localStorage.getItem('user');
         if (userData) {
             const { username } = JSON.parse(userData);
-            setUserName(username); // Set the username state
+            setUserName(username);
         }
     }, []);
 
@@ -47,47 +45,44 @@ const TheDash = () => {
     };
 
     return (
-        <div className="grid-sm">
-            
-            <h2>Welcome, <span className="accent">{userName}</span></h2>
-            {/* <h3>Dashboard</h3> */}
-            
-            <TheSidebar/>
-            {
-                showPopup && (
-                    <div className="popup">
-                        <div className="popup-content">
-                            <span className="close"  onClick={togglePopup}>&times;</span>
-                            <AddBudgetForm onAddBudget={handleAddBudget} />
+        <div className="container">
+            <TheSidebar />
+            <div className="main-content">
+                <div className="grid-sm">
+                    <h2>Welcome, <span className="accent">{userName}</span></h2>
+                    {showPopup && (
+                        <div className="popup">
+                            <div className="popup-content">
+                                <span className="close" onClick={togglePopup}>&times;</span>
+                                <AddBudgetForm onAddBudget={handleAddBudget} />
+                            </div>
                         </div>
-                    </div>
-                )
-            }
-            <div className="grid-lg">
-               
-
-                <h2>Existing Budgets</h2>
-                <div className="budgets">
-                    {budgets && budgets.length > 0 ? (
-                        budgets.map((budget) => (
-                            <BudgetItem key={budget.id} budget={budget} />
-                        ))
-                    ) : (
-                        <p>No budgets available.</p>
                     )}
-                </div>
-                <button className="btn btn--dark" style={{ width: '150px' }} onClick={togglePopup}>Add Budget</button>
-                {expenses && expenses.length > 0 && (
-                    <div className="grid-mid">
-                        <h2>Recent Expenses</h2>
-                        <Table expenses={expenses.sort((a, b) => b.createdAt - a.createdAt).slice(0, 8)} />
-                        {expenses.length > 5 && (
-                            <Link to="expenses" className="btn btn--dark">
-                                View all expenses
-                            </Link>
+                    <div className="grid-lg">
+                        <h2>Existing Budgets</h2>
+                        <div className="budgets">
+                            {budgets && budgets.length > 0 ? (
+                                budgets.map((budget) => (
+                                    <BudgetItem key={budget.id} budget={budget} />
+                                ))
+                            ) : (
+                                <p>No budgets available.</p>
+                            )}
+                        </div>
+                        <button className="btn btn--dark" style={{ width: '150px' }} onClick={togglePopup}>Add Budget</button>
+                        {expenses && expenses.length > 0 && (
+                            <div className="grid-mid">
+                                <h2>Recent Expenses</h2>
+                                <Table expenses={expenses.sort((a, b) => b.createdAt - a.createdAt).slice(0, 8)} />
+                                {expenses.length > 5 && (
+                                    <Link to="expenses" className="btn btn--dark">
+                                        View all expenses
+                                    </Link>
+                                )}
+                            </div>
                         )}
                     </div>
-                )}
+                </div>
             </div>
         </div>
     );
